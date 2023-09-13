2df30ca09f5ef06d9497d165b0bd1e59af26c41c
mat-daemon <mateuszsmigielski40@gmail.com>
2023-09-13 11:19:41 +0200

    Dummy piece test + impl movePIece

--- code/main.cpp
+++ code/main.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 
 #include "includes/Player.hpp"
+#include "includes/Piece.hpp"
 
 int main(){
     Player Hubert("Hubert");
     std::cout << Hubert << '\n';
     
-
+    Piece piece(10);
+    std::cout<< piece << "\n";
 }
--- code/source/IPiece.cpp
+++ code/source/IPiece.cpp
@@ -5,3 +5,7 @@ std::ostream& operator<<(std::ostream &out, const IPiece &piece) {
     out << "Pozycja: " << piece.square;
     return out;
 }
+
+void IPiece::movePiece(int position){
+    square = (square + position)%40; //TODO: magic number
+}
